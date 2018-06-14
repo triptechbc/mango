4fd9263a3873f27cdea0dbea0fcdfa4a53596747
Rhobox <Drazmataz@gmail.com>
2018-06-14 07:57:51 -0700

    Updated some front-end formatting

--- triptech_web_django/settings.py
+++ triptech_web_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%#dm(up&8q5f#goixlp)4hn*a9(m94f!t)9bk@yq%rslljim34'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.12']
 
 
 # Application definition
